be21ac2d3f4ae32d561f22ba9f16b92dac5ffb26
Alex Pierron <118303067+AP496@users.noreply.github.com>
2024-01-11 10:32:54 +0100

    Update save directory name in save_blip2_embs_imgs_v2.py

--- tools/embs/save_blip2_embs_imgs_v2.py
+++ tools/embs/save_blip2_embs_imgs_v2.py
@@ -74,14 +74,14 @@ if __name__ == "__main__":
 
     subdirectories = [subdir for subdir in args.image_dir.iterdir() if subdir.is_dir()]
     if len(subdirectories) == 0:
-        args.save_dir = args.image_dir.parent / f"blip-embs-{args.model_type}"
+        args.save_dir = args.image_dir.parent / f"blip-embs2-{args.model_type}"
         args.save_dir.mkdir(exist_ok=True)
         main(args)
     else:
         for subdir in subdirectories:
             args.image_dir = subdir
             args.save_dir = (
-                subdir.parent.parent / f"blip-embs-{args.model_type}" / subdir.name
+                subdir.parent.parent / f"blip-embs2-{args.model_type}" / subdir.name
             )
             args.save_dir.mkdir(exist_ok=True, parents=True)
             main(args)
